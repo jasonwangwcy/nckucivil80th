cb6f644924b318e8628803614ec3491f4300e6f0
Jason <chengyen030@gmail.com>
2024-09-19 11:05:21 +0800

    test vercel deploy

--- frontend/app/api/communities/route.ts
+++ frontend/app/api/communities/route.ts
@@ -5,10 +5,10 @@ const events = [
   { title: '登山健行', date: '2024-09-30', description: '台北山行活動', link:'https://www.ncku.edu.tw/'},
   { title: '科技論壇', date: '2024-10-10', description: '最新科技趨勢', link:'https://www.ncku.edu.tw/'},
   { title: '音樂欣賞', date: '2024-10-20', description: '校友音樂會', link:'https://www.ncku.edu.tw/'},
-  { title: '成大土木系友會活動2', date: '2024-09-21', description: '跨文化講座', link:'https://www.ncku.edu.tw/'},
-  { title: '登山健行2', date: '2024-09-30', description: '台北山行活動', link:'https://www.ncku.edu.tw/'},
-  { title: '科技論壇2', date: '2024-10-10', description: '最新科技趨勢', link:'https://www.ncku.edu.tw/'},
-  { title: '音樂欣賞2', date: '2024-10-20', description: '校友音樂會', link:'https://www.ncku.edu.tw/'},
+  { title: '成大土木系友會活動123', date: '2024-09-21', description: '跨文化講座', link:'https://www.ncku.edu.tw/'},
+  { title: '登山健行123', date: '2024-09-30', description: '台北山行活動', link:'https://www.ncku.edu.tw/'},
+  { title: '科技論壇123', date: '2024-10-10', description: '最新科技趨勢', link:'https://www.ncku.edu.tw/'},
+  { title: '音樂欣賞123', date: '2024-10-20', description: '校友音樂會', link:'https://www.ncku.edu.tw/'},
 ];
 
 export async function GET() {
