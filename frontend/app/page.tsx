dc1d4ef0ba93195b24a84605fd2f9b0c79605a0c
Jason <chengyen030@gmail.com>
2024-09-21 11:47:13 +0800

    fixed carousel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,11 +6,14 @@ import { Carousel,
     CarouselNext,
     CarouselPrevious,
     } from '../components/ui/carousel'; 
+
 import logo from '.././public/logo.png';
-import image1 from '.././public/1.jpg';
-import image2 from '.././public/2.jpg';
-import image3 from '.././public/3.jpg';
-import image4 from '.././public/4.jpg';
+import c1 from '.././public/a2.jpg';
+import c2 from '.././public/a4.jpg';
+import c3 from '.././public/a7.jpg';
+import c4 from '.././public/a8.jpg';
+import c5 from '.././public/aa.jpg';
+import c6 from '.././public/dance.jpg';
 import Image from 'next/image';
 
 export default function HomePage() {
@@ -40,29 +43,31 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="bg-red-100 p-4 h-80 md:h-96 lg:h-[30rem] overflow-hidden">
-  <Carousel className="h-full relative">
-    <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10" />
-    <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10" />
-    <CarouselContent className="h-full">
-      <CarouselItem className="flex justify-center items-center h-full">
-        <Image src={image1} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 h-auto object-contain mt-4" />
-      </CarouselItem>
-      <CarouselItem className="flex justify-center items-center h-full">
-        <Image src={image2} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 h-auto object-contain mt-4" />
-      </CarouselItem>
-      <CarouselItem className="flex justify-center items-center h-full">
-        <Image src={image3} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 h-auto object-contain mt-4" />
-      </CarouselItem>
-      <CarouselItem className="flex justify-center items-center h-full">
-        <Image src={image4} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 h-auto object-contain mt-4" />
-      </CarouselItem>
-      <CarouselItem className="flex justify-center items-center h-full">
-        <Image src={logo} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 h-auto object-contain mt-4" />
-      </CarouselItem>
-    </CarouselContent>
-  </Carousel>
-      
+      <div className="bg-red-100 p-4 h-50 md:h-96 lg:h-auto overflow-hidden">
+        <Carousel className="h-full relative">
+          <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10" />
+          <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10" />
+          <CarouselContent className="h-full">
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c1} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c2} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c3} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c4} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c5} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center items-center min-h-full">
+              <Image src={c6} alt="Logo" className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/2 h-auto object-contain transform scale-110" />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
       </div>
       <h1 className="text-3xl font-bold text-red-700 my-4">最新活動</h1>
       <div className="bg-white p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
