f62d3cee336e7c3647f5b53370e416692a1c9e8c
Jason <chengyen030@gmail.com>
2024-09-23 21:45:40 +0800

    add data into api

--- frontend/app/api/events/route.ts
+++ frontend/app/api/events/route.ts
@@ -6,8 +6,28 @@ const events = [
   { id:'3', title: '科技講座', date: '2024-10-10', description: '最新科技趨勢', image:"", link:'https://imgur.com/a/I1pSv1W' },
   { id:'4', title: '音樂會', date: '2024-10-20', description: '校友音樂會', image:"", link:'https://imgur.com/a/I1pSv1W' },
   { id:'5', title: '台北成大校友會 9/21活動2', date: '2024-09-21', description: '跨文化講座', image:"", link:'https://imgur.com/a/I1pSv1W'},
-  { id:'6', title: '登山健行活動2', date: '2024-09-30', description: '台北山行活動', image:"", link:'https://imgur.com/a/I1pSv1W'},
-  { id:'7', title: '科技講座2', date: '2024-10-10', description: '最新科技趨勢',image:"",  link:'https://imgur.com/a/I1pSv1W' },
+  { id:'6', 
+    title: 'Oktoberfest 德國啤酒節', 
+    date: '民國113年9月28日', 
+    description: '台北山行活動', 
+    image:{
+      src: "/images/beer.jpg",
+      width: 400,
+      height: 600,
+    }, 
+    link:'https://imgur.com/a/I1pSv1W'
+  },
+  { id:'7', 
+    title: '中區講座', 
+    date: '113年10月19日', 
+    description: '(confetti cup)成土飛揚、八方十洲(confetti cup)80周年系慶系列活動--中區講座重金邀請張金鶚得意門生、實價登錄始作俑者--成大統計系范垂爐學長以風趣的內容，為我們解說未來房產趨勢(open book)講座題目:看不到盡頭的房價(groom)主講人:范垂爐(Feb 14)時間:113年10月19日(六)  14:00~16:00(13:30報到)(cabin)地點:台中市北屯區文心路四段936號3樓_文心會議室_303室費用:免費名額:限量30名報名截止日期:113年9月30日主辦:成大土木系友會聯絡人:李梓賢 報名網址:https://forms.gle/3bEisbxUxmWicXTh9',
+    image:{
+      src: "/images/lecture.jpg",
+      width: 400,
+      height: 600,
+    },  
+    link:'https://imgur.com/a/I1pSv1W' 
+  },
   { id:'8', 
     title: '產業交流博覽會活動', 
     date: '民國113年11月9日', 
@@ -17,7 +37,8 @@ const events = [
       width: 400,
       height: 600,
     },
-    link:'https://imgur.com/a/I1pSv1W' , }
+    link:'https://imgur.com/a/I1pSv1W' , 
+  }
 ];
 
 export async function GET() {
