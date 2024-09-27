a400b9c189c6afbc4ddb3fdabd5069601db77f96
Jason <chengyen030@gmail.com>
2024-09-27 23:32:44 +0800

    fixed deploy

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,14 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { Carousel,
     CarouselContent,
     CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
     } from '../components/ui/carousel'; 
 
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
